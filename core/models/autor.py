65aa529a845b2e88baae346c96c2ac46e0df352c
Ana Heloiza <anaheloizadeoliveirab@gmail.com>
2025-04-15 16:50:39 -0300

    autor

---
+++ core/models/autor.py
@@ -0,0 +1,10 @@
+import email
+from django.db import models
+
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailFieldField(max_length=100)
+
+    def __str__(self):
+        return self.descricao

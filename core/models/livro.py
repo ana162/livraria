f242820d2654c345dfeaad2358a52dbec217268a
Ana Heloiza <anaheloizadeoliveirab@gmail.com>
2025-05-27 16:30:36 -0300

    feat: inclusão do relacionamento n para n entre Livro e Autor.

--- core/models/livro.py
+++ core/models/livro.py
@@ -14,3 +14,5 @@ class Livro(models.Model):
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
     def __str__(self):
         return f"({self.id}) {self.titulo} ({self.quantidade})"
+    from .autor import Autor
+    autores = models.ManyToManyField(Autor, related_name="livros", blank=True)

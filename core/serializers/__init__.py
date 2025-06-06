9c4370b6a4b59459e7047d3431b4aac1dd79de97
Ana Heloiza <anaheloizadeoliveirab@gmail.com>
2025-06-06 09:24:31 -0300

    feat: criação de dois serializadores para Livro.

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -2,8 +2,7 @@ from .user import UserSerializer
 from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
 from .livro import LivroSerializer
-
-
+from .livro import LivroListRetrieveSerializer, LivroSerializer
     
     
   
--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -9,6 +9,7 @@ class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = "__all__"
+        depth = 1
 
 class LivroSerializer(ModelSerializer):
     capa_attachment_key = SlugRelatedField(
@@ -23,3 +24,10 @@ class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = '__all__'
+
+
+class LivroListRetrieveSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1

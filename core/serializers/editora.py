435dac2b2ee7fc84e862610f0715d9476c17d2bf
ana162 <anaheloizadeoliveiab@gmail.com>
2025-09-02 16:34:02 -0300

    feat: validando e formatando dados antes de salvar

--- core/serializers/editora.py
+++ core/serializers/editora.py
@@ -1,3 +1,4 @@
+import email  # noqa: F401, I001
 from rest_framework.serializers import ModelSerializer
 
 from core.models import Editora
@@ -7,3 +8,6 @@ class EditoraSerializer(ModelSerializer):
     class Meta:
         model = Editora
         fields = "__all__"
+
+    def validate_email(self, email):  # noqa: E113, E301, E305, F811
+        return email.lower()  # noqa: E117

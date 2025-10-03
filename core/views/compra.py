d0663276e005bb4368b575aef946b5741f1a9451
ana162 <anaheloizadeoliveiab@gmail.com>
2025-10-03 09:31:34 -0300

    feat: finalizando a compra e atualizando a quantidade de itens em estoque

--- core/views/compra.py
+++ core/views/compra.py
@@ -1,4 +1,6 @@
+from django.db import transaction
 from django.utils import timezone
+from drf_spectacular.utils import extend_schema
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -42,3 +44,39 @@ class CompraViewSet(ModelViewSet):
         if usuario.groups.filter(name='administradores'):
             return Compra.objects.all()
         return Compra.objects.filter(usuario=usuario)
+
+    @action(detail=True, methods=["post"])
+    def finalizar(self, request, pk=None):
+        compra = self.get_object()
+
+        # Checa se a compra já foi finalizada
+        if compra.status != Compra.StatusCompra.CARRINHO:
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data={'status': 'Compra já finalizada'}
+            )
+
+        # Garante integridade transacional durante a finalização
+        with transaction.atomic():  # noqa: F821
+            for item in compra.itens.all():
+
+                # Valida se o estoque é suficiente para cada livro
+                if item.quantidade > item.livro.quantidade:
+                    return Response(
+                        status=status.HTTP_400_BAD_REQUEST,
+                        data={
+                            'status': 'Quantidade insuficiente',
+                            'livro': item.livro.titulo,
+                            'quantidade_disponivel': item.livro.quantidade,
+                        }
+                    )
+
+                # Atualiza o estoque dos livros
+                item.livro.quantidade -= item.quantidade
+                item.livro.save()
+
+            # Finaliza a compra: atualiza status
+            compra.status = Compra.StatusCompra.FINALIZADO
+            compra.save()
+
+        return Response(status=status.HTTP_200_OK, data={'status': 'Compra finalizada'})  # noqa: W292
